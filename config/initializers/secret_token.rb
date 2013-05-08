f57eba86802746fca169c64f22b6c97b530260f4
Bryan Chang <keunyoung.chang@gmail.com>
2013-05-08 10:43:17 -0700

    new initial commit for File Uploads Demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarrierwaveDemo::Application.config.secret_key_base = '8233f1b7e213448696fd4cdbe0601f065f36b7bcc35f5334367359f2f41949b11a7336b54f65679c74197922cbc1740048352555603fe99414807f1b58f0fa6a'
